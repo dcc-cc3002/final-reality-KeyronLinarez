ab021c801570f4c47e425271cc541d2aa33ddedd
KeyronLinarez <keyronlinarez@gmail.com>
2024-06-01 13:23:48 -0700

    fix: changed arrays in aParty from holding ACharacter to TCharacter

--- src/main/scala/TurnScheduler/aParty.scala
+++ src/main/scala/TurnScheduler/aParty.scala
@@ -10,14 +10,14 @@ import scala.collection.mutable.ListBuffer
 class aParty() {
   // make tuple containing characters
   //var party_list: (ListBuffer[AbstractCharacter]) = ListBuffer[AbstractCharacter]
-  var party_list: ListBuffer[AbstractCharacter] = ListBuffer.empty[AbstractCharacter]
+  var party_list: ListBuffer[Character] = ListBuffer.empty[Character]
   var party_size: Int = party_list.size
   // ensures a party can only have 3 members
   Require.Stat(party_size, "party size") atLeast 3
   // edit both lists individually
   //var (character_list, actionbar_list) = party_tuple
 
-  def addCharacter(character: AbstractCharacter): Unit = {
+  def addCharacter(character: Character): Unit = {
     party_list = party_list += character
     party_size += 1
 
@@ -29,7 +29,7 @@ class aParty() {
   // Update party_tuple with the modified lists
   //party_tuple = (character_list, actionbar_list)
 // Method to remove a character to the party
-def dropCharacter(character: AbstractCharacter): Unit = {
+def dropCharacter(character: Character): Unit = {
   // !! needs an if in case no action bar is passed
   // Find and remove the associated action bar for the character
   //actionbar_list = actionbar_list -= findActionBarForCharacter(character)
