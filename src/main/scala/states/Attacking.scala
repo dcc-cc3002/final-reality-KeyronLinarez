cffe5b0a1f80c902287fa7b6109b055be2136189
KeyronLinarez <keyronlinarez@gmail.com>
2024-07-09 20:30:42 -0700

    feat: added outline for attacking state

--- src/main/scala/states/Attacking.scala
+++ src/main/scala/states/Attacking.scala
@@ -1,16 +1,32 @@
 package states
 import gamecontroller.GameController
 
+/**
+ * The Attacking state in the game.
+ * This state handles the logic for characters attacking their targets.
+ */
 class Attacking extends AState {
+
+  /**
+   * Transitions to the next state.
+   * In this case, it transitions to the Resetting state.
+   *
+   * @param controller The game controller managing the state transitions.
+   */
   override def nextState(controller: GameController): Unit = {
     controller.setState(new Resetting)
   }
 
+  /**
+   * Handles the logic for the Attacking state.
+   * Processes attacks for all characters in the turn order queue until it's empty.
+   */
   override def handle(): Unit = {
-    //    + Hacer que una unidad ataque a otra.
-
-    //  + Hacer que un mago conjure un hechizo
-
+    scheduler.turnCheckAll()
+    while (scheduler.turnOrder.nonEmpty) {
+      val character = scheduler.turnOrder.dequeue()
+      // code missing to define target :(
+      character.attack(target)
+    }
   }
-
 }
